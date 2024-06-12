e4a5c3356156f8a445a2344a170d60a9966117c3
Vladyslav <vlama@inf.upv.es>
2024-06-13 01:55:39 +0200

    Second version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Vlamaz AI utils package'
 LONG_DESCRIPTION = 'A package that contains useful functions for AI and Data Science.'
 
@@ -21,14 +21,14 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['numpy', 'pandas'],
+    install_requires=['numpy', 'requests'],
     keywords=['python', 'machine learning', 'data', 'llama3', 'ai'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
